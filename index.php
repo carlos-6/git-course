348a40494866d3706f494f15e68aab18e3fe6b1c
sosa6 <carlos.sosa@porta.com.py>
2021-03-08 20:36:31 -0300

    comienzo del proyecto

---
+++ index.php
@@ -0,0 +1,10 @@
+!<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Git course</title>
+    </head>
+    <body>
+        <h1>Just a simple course</h1>
+    </body>
+</html>
